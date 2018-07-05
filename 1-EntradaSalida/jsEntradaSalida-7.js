136188158e1e64df1682ad45219d7f0c2e320a2e
eliias00 <36243813+eliias00@users.noreply.github.com>
2018-07-04 22:14:39 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -1,24 +1,47 @@
 /*Debemos lograr tomar Los numeros por ID ,
-transformarlos a enteros (parseInt),realizar la operación correcta y 
+transformarlos a enteros (parseInt),realizar la operación correcta y
 mostrar el resulto por medio de "ALERT"
-ej.: "la Resta es 750"*/ 
+ej.: "la Resta es 750"*/
 function sumar()
-{	
-	
+{
+	var num1
+	var num2
+	var suma
+	num1 = parseInt(document.getElementById("numeroUno").value);
+	num2 = parseInt(document.getElementById("numeroDos").value);
+	suma= num1+num2
+	alert("la suma es: " + suma);
 }
 
 function restar()
 {
-	
+	var num1
+	var num2
+	var resta
+	num1 = parseInt(document.getElementById("numeroUno").value);
+	num2 = parseInt(document.getElementById("numeroDos").value);
+	resta= num1-num2
+	alert("la resta es: " + resta);
 }
 
 function multiplicar()
-{ 
-	
+{
+	var num1
+	var num2
+	var mult
+	num1 = parseInt(document.getElementById("numeroUno").value);
+	num2 = parseInt(document.getElementById("numeroDos").value);
+	mult= num1*num2
+	alert("la multiplicacion es: " + mult);
 }
 
 function dividir()
 {
-	
+	var num1
+	var num2
+	var div
+	num1 = parseInt(document.getElementById("numeroUno").value);
+	num2 = parseInt(document.getElementById("numeroDos").value);
+	div= num1/num2
+	alert("la divicion es: " + div);
 }
-
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,10 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+var importe
+var nuevo
+importe = parseInt(document.getElementById("sueldo").value;
+nuevo= importe + 10
+document.getElementById("resultado").value=nuevo;
 
-	
 }
