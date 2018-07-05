88effe3791bca92bc6f84c7d17c0d0bb2be7d44d
eliias00 <36243813+eliias00@users.noreply.github.com>
2018-07-05 17:53:11 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,8 +1,15 @@
 function mostrar()
 {
-//tomo la edad  
+	var edad;
+	var ecivil
+	edad=document.getElementById("edad").value;
+	ecivil= document.getElementById("estadoCivil").value;
+if(edad >=18 && ecivil =="Soltero")
+	{
+		alert("es soltero y no es menor");
+	}
 
-	
+
 
 
 }//FIN DE LA FUNCIÓN
--- 8-TPs/jsFerretePinturas.js
+++ 8-TPs/jsFerretePinturas.js
@@ -2,12 +2,20 @@
 A.	Al ingresar una temperatura en Fahrenheit debemos mostrar la temperatura en Centígrados con un mensaje concatenado (ej.: " 32 Fahrenheit son 0 centígrados").
 B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en Fahrenheit (ej.: "0  centígrados son 32 Fahrenheit ").
 */
-function FahrenheitCentigrados () 
+function FahrenheitCentigrados ()
 {
-	
+	var temp
+	var temp2
+	temp = document.getElementById("Temperatura").value
+	temp2 = (temp-32)/1.8
+	alert(temp + " Fahrenheit son " + temp2 +  " centígrados ")
 }
 
-function CentigradosFahrenheit () 
+function CentigradosFahrenheit ()
 {
-	
+	var temp
+	var temp2
+	temp = document.getElementById("Temperatura").value
+	temp2 = temp*1.8 + 32
+	alert(temp + " centigrados son " + temp2 +  " Fahrenheit ")
 }
